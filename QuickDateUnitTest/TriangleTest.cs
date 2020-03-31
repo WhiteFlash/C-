551c44deb7bb021575902b4704ef626cedf61131
whiteFlash <ivankotov375@gmail.com>
2020-03-31 14:42:51 +1100

    Class tests methods for Triangle class. In order methods to work as expected.

---
+++ QuickDateUnitTest/TriangleTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickDate.Classes;
+using System;
+
+namespace QuickDateUnitTest
+{
+    [TestClass]
+    public class TriengleTest
+    {
+        [TestMethod]
+        public void CheckNumberTest()
+        {
+            //Arrange
+            int[] expected = { 1, 2, 3 };
+            var triangle_object = new Triangle();
+            var actual = triangle_object.CheckNumber(1, 3, 2);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual, $"Incorrect calues inside of array");
+        }
+
+        [TestMethod]
+        public void Check_Property()
+        {
+            //Arrenge
+            var triangle = new Triangle();
+            var expected = 5;
+            triangle.A = 5;
+
+            //Assert
+            Assert.AreEqual(expected, triangle.A, "Not equal");
+        }
+
+        [TestMethod]
+        public void Check_Existing_Method()
+        {
+            //Arrenge
+            var triangle = new Triangle(2,2,3);
+            var expexted = true;
+            var actual = triangle.CheckExisting();
+
+            //Assert
+            Assert.AreEqual(expexted, actual);
+        }
+
+        [TestMethod]
+        public void Check_GetToKnowType_Test()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var expected = "Pifagor";
+            var actual = triangle.GetToKnowType(3, 4, 5);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
