b258d6d4a60fb1b6cfe06785fd0cb2014c497249
whiteFlash <ivankotov375@gmail.com>
2020-07-29 11:17:09 +0300

    Test for the main function passed.

---
+++ QuickDateUnitTest/PlusMinusTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using QuickDate.Classes;
+
+namespace QuickDateUnitTest
+{
+    [TestClass]
+    public class PlusMinusTest
+    {
+        [TestMethod]
+        public void plusMinusTest()
+        {
+            //Arrange
+            string[] expected = { "0.333333", "0.333333", "0.333333" };
+            int[] toPass = { 1, -1, 0 };
+            var actual = PlusMinus.plusMinus(toPass);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual, $"Incorrect calues inside of array");
+        }
+    }
+}
